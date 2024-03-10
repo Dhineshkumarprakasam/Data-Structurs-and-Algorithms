b4149f82e8b7e0ff7501358c987d5ab48c25bbbc
Dhineshkumar <103942237+Dhineshkumarprakasam@users.noreply.github.com>
2024-03-10 10:26:42 +0530

    Update Valid_Brackets.c

--- Valid_Brackets.c
+++ Valid_Brackets.c
@@ -1,52 +1,48 @@
 #include <stdio.h>
 #include <string.h>
 
-int main()
+void main()
 {
+
     char exp[100];
     char stack[100];
-    int top = -1;
+    int top=-1;
 
-    printf("Enter your expression: ");
+    printf("Enter your exp : ");
     scanf("%s",&exp);
 
-    int len = strlen(exp);
+    int len=strlen(exp);
 
-    for (int i = 0; i < len; i++)
+    for(int i=0;i<len;i++)
+
     {
-        if (exp[i] == '(' || exp[i] == '[' || exp[i] == '{' || exp[i] == '<')
+        if(exp[i]=='(' || exp[i]=='[' || exp[i]=='{' || exp[i]=='<')
+            {
+                top++;
+                stack[top]=exp[i];
+            }
+        else if((exp[i]==')' || exp[i]==']' || exp[i]=='}' || exp[i]=='>') && top==-1)
         {
-            top++;
-            stack[top] = exp[i];
+            top-=1;
+            break;
         }
-        else if (exp[i] == ')' || exp[i] == ']' || exp[i] == '}' || exp[i] == '>')
+        else if(exp[i]==')' || exp[i]==']' || exp[i]=='}' || exp[i]=='>')
         {
-            if (top == -1)
-            {
-                top = -2;
-                break;
-            }
-            else if ((exp[i] == ')' && stack[top] == '('))
-                top--;
-            else if((exp[i] == ']' && stack[top] == '['))
-                top--;
-            else if((exp[i] == '}' && stack[top] == '{'))
-                top--;
-            else if((exp[i] == '>' && stack[top] == '<'))
-                top--;
+            if(stack[top]=='(' && exp[i]==')')
+                top-=1;
+            else if(stack[top]=='[' && exp[i]==']')
+                top-=1;
+            else if(stack[top]=='{' && exp[i]=='}')
+                top-=1;
+            else if(stack[top]=='<' && exp[i]=='>')
+                top-=1;
 
-            else
-            {
-                top = -2;
-                break;
-            }
         }
     }
 
-    if (top == -1)
+    if(top==-1)
         printf("It is valid\n");
     else
         printf("It is not valid\n");
 
-    return 0;
 }
